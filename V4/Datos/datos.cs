7c268d40b2f53a5c196a380d4cb6d9ec1632b693
PorfirioRodT <44884567+PorfirioRodT@users.noreply.github.com>
2020-10-03 00:52:42 -0400

    Add files via upload

---
+++ V4/Datos/datos.cs
@@ -0,0 +1,12 @@
+public class people{
+
+    public int id;
+    public string name;
+    public string lastName;
+    public int yearsOld;
+
+    public decimal savings;
+    public string password;
+    public string confirmPassword;
+
+}
